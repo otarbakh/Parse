983ac64e8ca57064e4bc09a5282e8eb7e3ecc3bc
otarbakh <otarbakh@gmail.com>
2022-08-10 02:37:47 +0400

    done,but have loading issue

--- app/src/main/java/com/example/parse/SecondDestination.kt
+++ app/src/main/java/com/example/parse/SecondDestination.kt
@@ -1,6 +1,9 @@
 package com.example.parse
 
+import android.graphics.Color
+import android.nfc.Tag
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -9,14 +12,19 @@ import androidx.fragment.app.viewModels
 import androidx.lifecycle.*
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.parse.databinding.FragmentSecondDestinationBinding
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
+
+const val Tag = "tag"
+
+@Suppress("DeferredResultUnused")
 
 class SecondDestination : Fragment() {
     private var _binding: FragmentSecondDestinationBinding? = null
     private val binding get() = _binding!!
     private val homeAdapter: Adapter by lazy { Adapter() }
 
-    val viewModel:SecondDestinationViewModel by viewModels()
+    val viewModel: SecondDestinationViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,27 +36,53 @@ class SecondDestination : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setupRecycler()
-        viewLifecycleOwner.lifecycleScope.launch() {
-            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
-                viewModel.data.collect {
-                    homeAdapter.submitList(it)
+
+        heandling()
+    }
+
+    private fun heandling() {
+
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.data.collectLatest {
+                    when (it) {
+                        is Resource.Success -> {
+                            homeAdapter.submitList(it.data)
+                            Log.d(Tag, "Success")
+                        }
+                        is Resource.Error -> {
+                            Log.d(tag, "${it.error}")
+                        }
+                        is Resource.Loader -> {
+                            if (it.isLoading) {
+                                binding.tvLoading
+
+                            } else {
+                                binding.rvRecycler.setBackgroundColor(Color.GREEN)
+                            }
+                        }
+                    }
                 }
             }
         }
-    }
 
-    private fun setupRecycler() {
-        binding.recycler.apply {
-            adapter = homeAdapter
-            layoutManager = LinearLayoutManager(requireContext(),LinearLayoutManager.VERTICAL,false)
-        }
-        viewModel.getHomesData()
-    }
 
+
+    }
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
+    private fun setupRecycler() {
+        binding.rvRecycler.apply {
+            adapter = homeAdapter
+            layoutManager =
+                LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+        }
+        viewModel.getMyData()
+
+    }
+
 
 }
--- app/src/main/java/com/example/parse/SecondDestinationViewModel.kt
+++ app/src/main/java/com/example/parse/SecondDestinationViewModel.kt
@@ -9,16 +9,34 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
 class SecondDestinationViewModel : ViewModel() {
-    private val _data = MutableStateFlow<List<HomeModel.Content>>(listOf())
+
+    private val _data =
+        MutableStateFlow<Resource<List<HomeModel.Content>>>(Resource.Loader(true))
     val data = _data.asStateFlow()
 
-    fun getHomesData() {
-        viewModelScope.launch(Dispatchers.IO) {
-            val response = RetrofitBuilder.getHomeModel().getHomeModel()
-            if (response.isSuccessful && response.body() != null) {
-                val data = response.body()
-                _data.emit(data?.content ?: emptyList())
+     suspend fun getMyData() {
+
+        _data.value = Resource.Loader(true)
+        val response = RetrofitBuilder.getHomeModel().getHomeModel()
+        response.body()?.let { data ->
+            when {response.isSuccessful -> {
+                _data.value = Resource.Success(data.content)
             }
+                else -> {
+                    val errorBody = response.errorBody()
+                    _data.value = Resource.Error(errorBody.toString())
+                }
+            }
+            _data.value = Resource.Loader(false)
         }
     }
+
+}
+
+
+
+sealed class Resource<T:Any> {
+    data class Success<T:Any>(val data: T) : Resource<T>()
+    data class Error<T:Any>(val error: String) : Resource<T>()
+    data class Loader<T:Any>(val isLoading: Boolean) : Resource<T>()
 }
